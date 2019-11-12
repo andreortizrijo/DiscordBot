c68296a4ad0b7554713aeb9780cf1202a346a980
Tomas1207 <tomasfernandes98@gmail.com>
2019-11-12 12:00:14 +0000

    Refactoring code

--- BOTS/twitchAPI/twitch.js
+++ BOTS/twitchAPI/twitch.js
@@ -9,8 +9,19 @@ module.exports = class TwichtBot {
     /**
      * @param {discord} client 
      */
+
+    //Discord Commands need
     constructor(client) {
         this.client = client;
+        //console.log(getServers(client));
+        var channel = this.client.guilds.get("567029802155114529").channels.find(channel => channel.name === "twicht_test");
+        console.log(channel);
+
+        if (channel == null) {
+            client.guilds.get("567029802155114529").createChannel("Twicht_Test", "text")
+        }
+        this.channel = channel;
+
     }
 
     async getUsers() {
@@ -26,32 +37,64 @@ module.exports = class TwichtBot {
             gamesID = gamesID + '&id=' + gamesList[index]
         }
         var gg = await axios.get(url + 'games?' + gamesID);
-        console.log(gg.data.data);
+        //console.log(gg.data.data);
         // Set the title of the field
-        var embed = new discord.RichEmbed();
-        embed.setTitle('The Most Streams views')
-            // Set the color of the embed
-            .setColor(0x6441a5)
 
-        embed.setDescription("***" + setstringsTogames(gg) + "***")
+
         var channel = this.client.guilds.get("567029802155114529").channels.find(channel => channel.name === "testes"); //Remover Hard Code VARS
-        await channel.send(embed)
+        //console.log(channel);
+
+        await channel.send(setEmbed(setstringsTostreams, twitchResponse, "Top Streams on Twitch"));
 
     };
 
     async getTopGames() {
         const gg = await axios.get(url + 'games/top');
-        console.log(gg.data.data);
+        //console.log(gg.data.data);
         // Set the title of the field
-        var embed = new discord.RichEmbed();
-        embed.setTitle('Most Played Games in twicht')
-            // Set the color of the embed
-            .setColor(0x6441a5)
-        var channel = this.client.guilds.get("567029802155114529").channels.find(channel => channel.name === "testes");
 
-        embed.setDescription("***" + setstringsTogames(gg) + "***")
-        await channel.send(embed)
+        let list = getServers(this.client);
+        //console.log(list["Streamz"]);
+
+        var channel = this.client.guilds.get(list["Streamz"]).channels.find(channel => channel.name === "testes");
+
+        await channel.send(setEmbed(setstringsTogames, gg, "Top Games on Twitch"))
     }
+}
+setEmbed = function (srtingFunction, response, title) {
+    var embed = new discord.RichEmbed();
+    embed.setTitle(title)
+        // Set the color of the embed
+        .setColor(0x6441a5);
+
+    embed.setDescription("***" + srtingFunction(response) + "***")
+
+
+    return embed;
+}
+
+/**
+ * @param {discord} client
+ * @returns {discord.guild}
+ */
+getServers = function (client) {
+    let ListOfServes = [];
+    for (var [key, value] of client.guilds) {
+        ListOfServes[value] = key;
+    }
+    return ListOfServes;
+}
+
+/**
+ * @param {discord} client
+ * @returns {int}
+ */
+getServersLenght = function (client) {
+    var index = 0;
+    for (var key of client.guilds) {
+        index++;
+    }
+    return index;
 }
 /**
  * @param {AxiosResponse} games 
@@ -60,7 +103,15 @@ setstringsTogames = function (games) {
     var fullstringOfGames = games.data.data[0].name + "\n";
     for (let index = 1; index < games.data.data.length; index++) {
         var element = games.data.data[index].name;
-        fullstringOfGames = fullstringOfGames + element + "\n"
+        fullstringOfGames = fullstringOfGames + element + "\n";
+    }
+    return fullstringOfGames;
+}
+setstringsTostreams = function (streams) {
+    var fullstringOfGames = streams.data.data[0].user_name + "\n";
+    for (let index = 1; index < streams.data.data.length; index++) {
+        var element = streams.data.data[index].user_name;
+        fullstringOfGames = fullstringOfGames + element + "\n";
     }
     return fullstringOfGames;
 }
--- index.js
+++ index.js
@@ -20,23 +20,6 @@ client.on('ready', () => {
   twichtBot.getUsers();
 });
 
-client.on('message', message => {
-  // If the message is "how to embed"
-  if (message.content === 'how to embed') {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-    const embed = new RichEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xFF0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!' + message.author);
-    // Send the embed to the same channel as the message
-    message.channel.send(embed);
-  }
-});
 client.on("error", (e) => console.error(e));
 client.on("warn", (e) => console.warn(e));
 client.on("debug", (e) => console.info(e));
