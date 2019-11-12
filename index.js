faa47976c5fb029f2d14a36ee2a3a9e779e0de7f
André Rijo <andreortizrijo@gmail.com>
2019-11-12 17:56:51 +0000

    Optimização do código

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { Client, Attachment,RichEmbed  } = require('discord.js');
+const InviteBot = require('./BOTS/invite/createInvite.js')
 
 // Create an instance of a Discord client
 const client = new Client();
+let inviteBot = null;
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -9,23 +11,13 @@ const client = new Client();
  */
 client.on('ready', () => {
   console.log('I am ready!');
+  inviteBot = new InviteBot(client);
 });
 
 client.on('message', message => {
-    // If the message is "how to embed"
-    if (message.content === 'how to embed') {
-      // We can create embeds using the MessageEmbed constructor
-      // Read more about all that you can do with the constructor
-      // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-      const embed = new RichEmbed()
-        // Set the title of the field
-        .setTitle('A slick little embed')
-        // Set the color of the embed
-        .setColor(0xFF0000)
-        // Set the main content of the embed
-        .setDescription('Hello, this is a slick embed!' + message.author);
-      // Send the embed to the same channel as the message
-      message.channel.send(embed);
+    if (message.content === '!create invite'){
+      inviteBot.replyWithInvite(message);
     }
   });
+
 client.login('NjA0NzU2MjA1MzM2MDAyNjEz.XamDDA.0BBxi7u4YW9U1WC7cyW_Tr5TeWs');
