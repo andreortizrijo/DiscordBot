338b107d983933f03cbdca71424a24f5febf3cb6
André Rijo <andrerijo@Andres-MacBook-Pro.local>
2019-11-13 11:43:13 +0000

    Merge with create_invite branch

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const { Client, Attachment,RichEmbed  } = require('discord.js');
+const InviteBot = require('./BOTS/invite/createInvite.js')
 const PMBot = require('./BOTS/pm/sendPM.js')
 
 // Create an instance of a Discord client
 const client = new Client();
-let pmBot = null;
-const InviteBot = require('./BOTS/invite/createInvite.js')
-
-// Create an instance of a Discord client
-const client = new Client();
 let inviteBot = null;
+let pmBot = null;
 
 /**
  * The ready event is vital, it means that only _after_ this will your bot start reacting to information
@@ -16,20 +13,18 @@ let inviteBot = null;
  */
 client.on('ready', () => {
   console.log('I am ready!');
+  inviteBot = new InviteBot(client);
   pmBot = new PMBot(client);
 });
 
 client.on('message', message => {
-    if (message.content.startsWith('!pm')) {
-      pmBot.sendPrivateMessage(message);
-    }
-    inviteBot = new InviteBot(client);
+  if (message.content === '!create invite'){
+    inviteBot.replyWithInvite(message);
+  }
+  
+  if (message.content.startsWith('!pm')) {
+    pmBot.sendPrivateMessage(message);
+  }
 });
-
-client.on('message', message => {
-    if (message.content === '!create invite'){
-      inviteBot.replyWithInvite(message);
-    }
-  });
 
 client.login('NjA0NzU2MjA1MzM2MDAyNjEz.XamDDA.0BBxi7u4YW9U1WC7cyW_Tr5TeWs');
