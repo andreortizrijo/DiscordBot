fd2cb0895c36abc2f4dbd107c50ab294fa53fbfd
André Rijo <andreortizrijo@gmail.com>
2019-11-14 19:46:22 +0000

    Still in tests

--- BOTS/Ban&Kick/band_kick.js
+++ BOTS/Ban&Kick/band_kick.js
@@ -1,22 +1,24 @@
 const { Client, Attachment,RichEmbed,GuildMember  } = require('discord.js');
 
 let memberList = [];
+let kickLog = null;
 
 module.exports = class BandAndKick{
 
     constructor(client){
         this.client = client;
     }
-  
+
     async memberKick(message) {
-        message.mentions.members.forEach(function(value, key) {
-            console.log(key + ' = ' + value);
+        await message.mentions.members.forEach(function(value, key) {
+            memberList.push(key,value);
         });
 
-        console.log('First Member Mentioned: ' + message.mentions.members.first());
+        console.log(memberList.length);
 
-        for (let i = 0; i < memberList.length; i++) {
-            memberList[i].kick().then(() => message.reply(`Kicked ${member.displayName}`)).catch(console.error);     
+        for (let i = 1; i < memberList.length; i++) {
+            //Kick member
+            message.guild.member(memberList[i]).kick().then(() => message.reply(`Kicked ${memberList[i].displayName}`)).catch(console.error);
         }
     }
-  };    
+};
